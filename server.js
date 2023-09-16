b5d7c70612485bd2ef40c8ff7ee4ce63d101d363
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-16 13:33:33 +0530

    get other req method code

--- server.js
+++ server.js
@@ -3,13 +3,43 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5001;
 
+// simple server creation----
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "application/json");
+//   res.write(
+//     JSON.stringify({ message: "Hello I m your server, Up and Running!" })
+//   );
+//   res.end();
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running on PORT: ${PORT}`);
+// });
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.write(
-    JSON.stringify({ message: "Hello I m your server, Up and Running!" })
-  );
-  res.end();
+  switch (req.method) {
+    case "GET":
+      getReq(req, res);
+      break;
+    case "POST":
+      postReq(req, res);
+      break;
+    case "PUT":
+      putReq(req, res);
+      break;
+    case "DELETE":
+      deleteReq(req, res);
+      break;
+    default:
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({ title:"Not Found", message: "Route not found!" })
+      );
+      res.end();
+      break;
+  }
 });
 
 server.listen(PORT, () => {
